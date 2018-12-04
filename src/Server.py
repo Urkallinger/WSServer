99c8be59fe74ce1d496e00994502a5bd15a73755
Philipp <x@y.de>
2018-12-04 10:47:12 +0100

    Renamed Main to Server

--- src/Server.py
+++ src/Server.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-
 @socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-
+def handleMessage(msg):
+    print('Message: {}'.format(msg))
+    send(msg, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
