f0698a21f72fdb24a3e226d1c94a2edaee659e97
Philipp <x@y.de>
2018-12-04 13:57:12 +0100

    Server as class

--- src/Server.py
+++ src/Server.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO, send
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message: {}'.format(msg))
-    send(msg, broadcast=True)
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.config['SECRET_KEY'] = 'secret!'
+        self.socketio = SocketIO(self.app)
+
+        self.registerEvents()
+
+    def run(self):
+        self.socketio.run(self.app)
+
+    def registerEvents(self):
+        self.socketio.on_event('message', Server.handleMessage)
+
+    @staticmethod
+    def handleMessage(msg):
+        print('Message: {}'.format(msg))
+        send(msg, broadcast=True)
+
 
 if __name__ == '__main__':
-    socketio.run(app)
+    Server().run()
