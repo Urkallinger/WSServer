6dabeb20cdca6ce9bc1d49bdcaa89197a6b44100
Philipp <x@y.de>
2018-12-04 17:55:04 +0100

    added new event "getUsers"

--- src/WsServer.py
+++ src/WsServer.py
@@ -1,5 +1,9 @@
+import json
+
 from flask import Flask
 from flask_socketio import SocketIO, emit
+
+from database.MongoClient import MongoClient
 
 
 class WsServer:
@@ -10,8 +14,10 @@ class WsServer:
         self.app = Flask(__name__)
         self.app.config['SECRET_KEY'] = 'secret!'
         self.socketIO = SocketIO(self.app)
+        self.mongoClient = MongoClient()
 
         self.socketIO.on_event('broadcast', self.handleBroadcast)
+        self.socketIO.on_event('getUsers', self.getUsers)
 
     def run(self):
         """
@@ -25,6 +31,16 @@ class WsServer:
         """
         print('broadcast: {}'.format(msg))
         emit('broadcast', msg, broadcast=True)
+
+    def getUsers(self):
+        """
+        Returns all users.
+        """
+        users = self.mongoClient.getUsers()
+        data = json.dumps(users)
+        print(data)
+        emit('getUsers', data)
+
 
 
 if __name__ == '__main__':
